bc2a527a1ebfd85451308472d24c8c5935a9715d
Dhruv Wadhwani <dhruv@Dhruvs-MacBook-Air.local>
2025-11-29 18:39:19 +0530

    custom hooks

---
+++ 06_currency_converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useState,useEffect } from "react";
+
+function useCurrencyInfo(currency){
+    // i want that only the api calls when this function runs or user calls then only 
+
+    const [data,setData] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+        console.log(data)
+
+    },[currency])
+    console.log(data)
+    return data
+    // return [data,setData]
+}
+
+export default useCurrencyInfo;
+
+//api call https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json
